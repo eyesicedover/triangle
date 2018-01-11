887dc4d66aff2461e99c4bc0fd9b472468e260e3
Chase Bowden and Stephanie Faber <chase@>
2018-01-11 14:18:09 -0800

    add html form and jquery to compare input

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $('#triForm').submit(function(event){
+    var tri1 = parseInt($("input#side1").val());
+    var tri2 = parseInt($("input#side2").val());
+    var tri3 = parseInt($("input#side3").val());
+
+    $("p").hide();
+    if(tri1 === tri2 && tri1 === tri3) {
+      $("p#equilateral").show();
+    } else if (tri1 === tri2 || tri2 === tri1 || tri1 ===tri3) {
+      $("p#isosceles").show();
+    } else if ((tri1+tri2) > tri3 || (tri2+tri3) > tri1 || (tri1+tri3) > tri2) {
+      $("p#notTri").show();
+    } else {
+      $("p#scalene").show();
+    }
+    alert(tri1);
+    event.preventDefault();
+  });
+
+
+
+});
